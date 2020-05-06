9363883446773f5e5a511b67723cd227fdcde88d
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2020-05-06 23:52:38 +0300

    file reading was added

--- src/service/cli/generate/index.js
+++ src/service/cli/generate/index.js
@@ -3,10 +3,18 @@
 const chalk = require(`chalk`);
 const fs = require(`fs`);
 const util = require(`util`);
-const {getRandomInt, shuffle} = require(`../../utils`);
-const titles = require(`./content/titles.json`);
-const categories = require(`./content/categories.json`);
-const textСhunks = require(`./content/text-chunks.json`);
+const {
+  getRandomInt,
+  shuffle,
+  readContent,
+} = require(`../../utils`);
+
+
+const {
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
+  FILE_CATEGORIES_PATH,
+} = require(`../../../constants`);
 
 const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
@@ -14,7 +22,7 @@ const FILE_NAME = `mocks.json`;
 
 const CategorySizeRestrict = {
   MIN: 1,
-  MAX: Math.min(3, categories.length),
+  MAX: Math.min(3, 5),
 };
 const AnnounceRestrict = {
   MIN: 1,
@@ -25,19 +33,22 @@ const FullTextRestrict = {
 };
 const THREE_MONTHS = 3 * 30 * 24 * 60 * 60 * 1000;
 
-const generateCategories = (count) => shuffle(categories).slice(0, count);
+const generateCategories = (count, categories) => shuffle(categories).slice(0, count);
 const getRandomSubList = (list = [], min = 0, max = list.length) => (
   shuffle(list).slice(0, getRandomInt(min, max))
 );
 const getRandomDate = (minTime, maxTime) => new Date(getRandomInt(minTime, maxTime));
 
-const generateOffers = (count) => (
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
     title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: getRandomDate(Date.now() - THREE_MONTHS, Date.now()).toISOString(),
-    announce: getRandomSubList(textСhunks, AnnounceRestrict.MIN, AnnounceRestrict.MAX).join(` `),
-    fullText: getRandomSubList(textСhunks, FullTextRestrict.MIN).join(` `),
-    category: generateCategories(getRandomInt(CategorySizeRestrict.MIN, CategorySizeRestrict.MAX)),
+    announce: getRandomSubList(sentences, AnnounceRestrict.MIN, AnnounceRestrict.MAX).join(` `),
+    fullText: getRandomSubList(sentences, FullTextRestrict.MIN).join(` `),
+    category: generateCategories(
+        getRandomInt(CategorySizeRestrict.MIN, CategorySizeRestrict.MAX),
+        categories,
+    ),
   }))
 );
 
@@ -55,10 +66,23 @@ module.exports = {
   name: `--generate`,
   description: `формирует файл mocks.json`,
   async run(count) {
-    const countOffer = getOfferCount(count, DEFAULT_COUNT, MAX_COUNT);
-    const content = JSON.stringify(generateOffers(countOffer), null, 2);
+    try {
+      const countOffer = getOfferCount(count, DEFAULT_COUNT, MAX_COUNT);
+      const titles = await readContent(FILE_TITLES_PATH);
+      const categories = await readContent(FILE_CATEGORIES_PATH);
+      const sentences = await readContent(FILE_SENTENCES_PATH);
 
-    await writeFile(FILE_NAME, content);
-    console.info(chalk.green(`Operation success. File created.`));
+      const content = JSON.stringify(generateOffers(
+          countOffer,
+          titles,
+          categories,
+          sentences,
+      ), null, 2);
+
+      await writeFile(FILE_NAME, content);
+      console.info(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
   },
 };
